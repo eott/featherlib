2a8039f7cbb86d0fbbda77e429843882ecc6de80
Etienne Ott <eti.ott@gmail.com>
2015-10-18 11:29:56 +0200

    Initial commit. Create new Visual Studio project and import core schema.

---
+++ schema/core_structure.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS `library`;
+
+CREATE TABLE `library` (
+  `library_id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`library_id`),
+  KEY `owner` (`owner_id`),
+  CONSTRAINT `owner` FOREIGN KEY (`owner_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `library_item`;
+
+CREATE TABLE `library_item` (
+  `library_item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `library_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`library_item_id`),
+  KEY `library` (`library_id`),
+  CONSTRAINT `library` FOREIGN KEY (`library_id`) REFERENCES `library` (`library_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
