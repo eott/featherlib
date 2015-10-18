82c2885f913aa2e8fb474e158f7f8f63b0da0b22
Etienne Ott <eti.ott@gmail.com>
2015-10-18 13:08:29 +0200

    Implement menu exit button.

--- featherlib/Main.cs
+++ featherlib/Main.cs
@@ -16,5 +16,10 @@ namespace featherlib
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
