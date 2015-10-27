408bbd9b2d2d410002d1d84ba96f2b2bc0cdddbe
Etienne Ott <eti.ott@gmail.com>
2015-10-27 19:42:14 +0100

    Add skeleton class to represent a library item.

---
+++ featherlib/LibraryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace featherlib
+{
+    class LibraryItem
+    {
+        protected int libraryItemId;
+
+        protected Library library;
+
+        protected string name;
+
+        public LibraryItem(int id, Library library, string name)
+        {
+            this.libraryItemId = id;
+            this.library = library;
+            this.name = name;
+        }
+    }
+}
