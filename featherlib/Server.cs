1284f7ff9f3d3c3c4b53b79cc820412fa57b85c6
Etienne Ott <eti.ott@gmail.com>
2015-11-04 10:27:08 +0100

    Add Server skeleton class to represent the data persistency layer and wrap the database away from the client side.

---
+++ featherlib/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace featherlib
+{
+    class Server
+    {
+        protected DbConnection db;
+
+        public Server(DbConnection db)
+        {
+            this.db = db;
+        }
+    }
+}
