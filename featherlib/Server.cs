b7d661a25da7c3b8f0c58cd66de8070e96865ff6
Etienne Ott <eti.ott@gmail.com>
2015-11-07 10:29:59 +0100

    Add method getDataRederFor to class server to support ServerConnector instances with direct access to the server.

--- featherlib/Server.cs
+++ featherlib/Server.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,39 @@ namespace featherlib
         public Server(DbConnection db)
         {
             this.db = db;
+        }
+
+        public MySqlDataReader getDataReaderFor(ObjectIdentifier ident, Dictionary<string, string> parameters)
+        {
+            string sql = "";
+            Dictionary<string, string> pars = new Dictionary<string, string>();
+
+            switch (ident)
+            {
+                case ObjectIdentifier.Library:
+                    if (parameters.ContainsKey("id"))
+                    {
+                        sql = @"
+                            SELECT owner_id, created, name
+                            FROM library l
+                            WHERE l.library_id = :id
+                        ";
+                        pars["id"] = parameters["id"];
+                    }
+                    else
+                    {
+                        sql = @"
+                            SELECT owner_id, created, name
+                            FROM library l
+                        ";
+                    }
+                    break;
+
+                default:
+                    throw new NotSupportedException("Given ObjectIdentifier is not supported.");
+            }
+
+            return this.db.query(sql, pars);
         }
     }
 }
