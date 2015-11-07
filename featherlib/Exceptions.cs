23577fd620bb6d1eed0eed6eefcb42f2c58b537f
Etienne Ott <eti.ott@gmail.com>
2015-11-07 12:58:53 +0100

    Add file with Exception classes.

---
+++ featherlib/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace featherlib
+{
+    public class EmptyResultSetException : Exception
+    {
+        public EmptyResultSetException(string msg) : base(msg) { }
+    }
+}
